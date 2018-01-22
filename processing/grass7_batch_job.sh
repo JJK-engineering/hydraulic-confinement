9e45fd0cecbb579e3f93cad466e70291db5aa1f7
Joseph Kaelin <jjk@jkaelin.ch>
2018-01-22 20:41:18 +0000

    hydraulic confinement work including code refactoring

--- processing/grass7_batch_job.sh
+++ processing/grass7_batch_job.sh
@@ -1,6 +1,6 @@
-g.proj -c proj4="+proj=longlat +datum=WGS84 +no_defs"
-v.in.ogr min_area=0.0001 snap=-1 input="tmp" layer=TestAlignmentLine output=tmp1516308553832 --overwrite -o
-g.region n=1274890 s=1260650 e=2624270 w=2603510 res=100
-v.to.points  input="tmp1516308553832" dmax="100" use=vertex -i output=output0e592ae5944d4843ae39c2ba7741ac61 --overwrite
-v.out.ogr -s -e input=output0e592ae5944d4843ae39c2ba7741ac61 type=auto output="tmp" format=ESRI_Shapefile output_layer=TestAlignmentPoints --overwrite
+g.proj -c proj4="+proj=omerc +lat_0=46.95240555555556 +lonc=7.439583333333333 +alpha=90 +k=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +units=m +no_defs"
+r.external input="data/swissALTI3D_.tif" band=1 output=tmp1516653597115 --overwrite -o
+v.in.ogr min_area=0.0001 snap=-1 input="tmp" layer=TestBufferFinal output=tmp1516653597116 --overwrite -o
+g.region n=1274890 s=1260650 e=2624270 w=2603510 res=5.0
+r.what  map="tmp1516653597115" points="tmp1516653597116" null_value="NA" separator="," cache="500" -n > tmp/BufferDTM.csv --overwrite
 exit
