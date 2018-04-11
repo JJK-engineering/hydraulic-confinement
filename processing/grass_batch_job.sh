de735db93ce25bb5373fac3d8cba51cda32ee411
Joseph Kaelin <jjk@jkaelin.ch>
2018-04-11 13:54:13 +0000

    added large geospatial files to .gitignore

--- processing/grass_batch_job.sh
+++
@@ -1,3 +0,0 @@
-g.region rast=basina80bc78206a24ee19caeacc431e9209f
-r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=basina80bc78206a24ee19caeacc431e9209f output="/tmp/processing354b244baa0c4661a5f1de77564950c0/32a234c20f53487396d140b6eb11c272/basin.tif"
-exit
